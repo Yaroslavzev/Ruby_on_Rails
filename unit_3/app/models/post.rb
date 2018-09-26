eb9d81ded5e689e65c9d7c4f747c4e90ec4893b4
Yaroslavzev <yaroslavzevalex@gmail.com>
2018-09-26 14:56:57 +0300

    Task_1

--- unit_3/app/models/post.rb
+++ unit_3/app/models/post.rb
@@ -1,3 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user
+  validates :title, presence: true
+  validates :title, lenght: { maximum:25, minimum: 5}
+  validates :body, presence: true
+  validates :body, lenght: { minimum: 5}
 end
