af847d1161ce210648b18b5a5a5f147ad98406ee
Yaroslavzev <yaroslavzevalex@gmail.com>
2018-10-08 15:28:44 +0500

    Tasks from manual

--- unit_6/config/application.rb
+++ unit_6/config/application.rb
@@ -18,6 +18,8 @@ Bundler.require(*Rails.groups)
 
 module Blog
   class Application < Rails::Application
+    config.i18n.default_locale = :ru
+    config.time_zone  = 'Moscow'
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
