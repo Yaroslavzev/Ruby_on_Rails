22641b70a5e7bb177e44fe937da1dfe09c222121
Yaroslavzev <yaroslavzevalex@gmail.com>
2018-10-11 15:46:52 +0300

    application was changed. Foreman added

--- unit_7/app/javascript/packs/application.js
+++ unit_7/app/javascript/packs/application.js
@@ -7,4 +7,7 @@
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
 
-console.log('Hello World from Webpacker')
+import  "./application.css"
+window.onload  =  function (){
+  document.body.insertAdjacentHTML ( "afterbegin" ,  "Hello Webpack!" );
+};
